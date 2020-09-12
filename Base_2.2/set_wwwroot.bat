64e38efea4b8ba5f520638d935e4f343bf91737e
thony <a0982830615@gmail.com>
2020-09-12 11:30:27 +0800

    Add

--- Base_2.2/set_wwwroot.bat
+++ Base_2.2/set_wwwroot.bat
@@ -1,2 +1,5 @@
-mklink /d  D:\A\Code\Lab\NetCore_MVC\Base_2.2\Base_2.2\".\wwwroot" D:\A\Code\Lab\NetCore_MVC\wwwroot\wwwroot
+set cur_path=D:\A\Code\github\NetCore_MVC\Base_2.2\Base_2.2
+set path=%cur_path%\..\..\wwwroot\wwwroot
+echo %path%
+mklink /d  %cur_path%\wwwroot %path%
 pause
